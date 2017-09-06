d8aa96d1ebbaccf2f83da26aa49ad8575ec401ac
Selwyn <talk@selwyn.cc>
2017-09-06 21:07:04 +0200

    Add alias to open the working directory in Atom

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 alias mydotfiles='git --git-dir=$HOME/.my-dotfiles/ --work-tree=$HOME'
 
 # General
+alias at='atom .'
 alias doh='commandline -i "sudo $history[1]"; history --delete "$history[1]"'
 alias ll='ls -l --almost-all --human-readable'
 
