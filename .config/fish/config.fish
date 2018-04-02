e825dabff9cf0689a8669bcd1c27acc17c5a6c23
Selwyn <talk@selwyn.cc>
2018-04-02 12:47:31 +0200

    Remove unused `cs` function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,12 +18,6 @@ alias at='atom .'
 alias doh='commandline -i "sudo $history[1]"; history delete --exact --case-sensitive "$history[1]"'
 alias ll='ls -l --almost-all --human-readable'
 
-# List contents after moving to given directory
-function cs
-  cd $argv
-  ls -A
-end
-
 # npm
 alias nps='npm start'
 alias npi='npm install'
