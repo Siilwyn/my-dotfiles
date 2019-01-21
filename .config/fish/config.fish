c9b3500df9e5609b4f5a61ed903b6d8895feed39
Selwyn <talk@selwyn.cc>
2019-01-21 13:27:04 +0100

    Remove unused atom alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 set fish_greeting
 
 # Set environment variables
-set -x CARGO_HOME ~/.cache/cargo
 set -x NODE_ENV development
 set -x NODE_REPL_HISTORY ~/.cache/.node-repl
 set -x NPM_CONFIG_CACHE ~/.cache/npm
 set -x NPM_CONFIG_USERCONFIG ~/.config/npmrc
+
+set -x CARGO_HOME ~/.cache/cargo
 set -x RUSTUP_HOME ~/.cache/rustup
 
 set -x PATH $PATH ~/.local/bin $CARGO_HOME/bin
@@ -14,7 +15,6 @@ set -x PATH $PATH ~/.local/bin $CARGO_HOME/bin
 alias mydotfiles='git --git-dir=$HOME/.my-dotfiles/ --work-tree=$HOME'
 
 # General
-alias at='atom .'
 alias doh='commandline -i "sudo $history[1]"; history delete --exact --case-sensitive "$history[1]"'
 alias ll='ls -l --almost-all --human-readable'
 
