31d984b38dd2ab00879881c76649937f5a617593
Selwyn <talk@selwyn.cc>
2020-12-27 13:59:24 +0100

    Remove customization no longer needed with npm 6 defaults

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,6 @@ set -x NODE_ENV development
 set -x NODE_REPL_HISTORY ~/.cache/.node-repl
 set -x NPM_CONFIG_CACHE ~/.cache/npm
 set -x NPM_CONFIG_USERCONFIG ~/.config/npmrc
-set -x NPM_CONFIG_PREFER_OFFLINE true
-set -x NPM_CONFIG_OPTIONAL false
 
 set -x CARGO_HOME ~/.cache/cargo
 set -x RUSTUP_HOME ~/.cache/rustup
@@ -26,5 +24,4 @@ alias vi='nvim'
 alias nps='npm start'
 alias npi='npm install'
 alias npt='npm test'
-alias npl='npm ls --depth 0'
 alias npr='npm run'
