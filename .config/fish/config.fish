110a11af0e6647551084d8aca198ebd55901ffde
Selwyn <talk@selwyn.cc>
2025-04-27 15:32:15 +0200

    Switch from Volta to mise-en-place

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ set -x XDG_CONFIG_HOME ~/.config
 # XDG list of shame
 set -x RUSTUP_HOME ~/.cache/rustup
 set -x CARGO_HOME ~/.cache/cargo
-set -x VOLTA_HOME ~/.cache/volta
 set -x NODE_REPL_HISTORY ~/.cache/.node-repl
 set -x NPM_CONFIG_CACHE ~/.cache/npm
 set -x NPM_CONFIG_USERCONFIG ~/.config/npmrc
@@ -18,15 +17,15 @@ set -x NPM_CONFIG_UPDATE_NOTIFIER false
 set -x GH_NO_UPDATE_NOTIFIER true
 
 set -x EDITOR vi
-set -x PATH $VOLTA_HOME/bin $CARGO_HOME/bin $RUSTUP_HOME/toolchains/*/bin $GOPATH/bin $PATH
-
+set -x PATH $CARGO_HOME/bin $RUSTUP_HOME/toolchains/*/bin $GOPATH/bin $PATH
+mise activate fish --shims | source
 # Manage my dotfiles
 alias mydotfiles='git --git-dir=$HOME/.my-dotfiles/ --work-tree=$HOME'
 
 # General
 alias doh='commandline -i "sudo $history[1]"; history delete --exact --case-sensitive doh'
 alias ll='ls -l --almost-all --human-readable'
-alias listapps='dnf list --installed | grep @@commandline; flatpak list --app; volta list; cargo install --list'
+alias listapps='dnf list --installed | grep @@commandline; flatpak list --app; mise list --global; cargo install --list'
 
 # npm
 alias nps='npm start'
